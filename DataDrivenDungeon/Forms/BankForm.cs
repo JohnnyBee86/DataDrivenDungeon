da149402f2eaa0c0dd0e746a04e1a17181293817
Elias McKee <eqbookfan@gmail.com>
2023-07-26 14:20:42 -0700

    Created a folder for all of the views, and added the forms for the bank and the dungeon.

---
+++ DataDrivenDungeon/Forms/BankForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataDrivenDungeon
+{
+    public partial class BankForm : Form
+    {
+        /// <summary>
+        /// This is the form for the Bank, where the player can store and take out coins and magic items,
+        /// so that if they fail in a dungeon, they won't lose the items in the bank.
+        /// </summary>
+        public BankForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ DataDrivenDungeon/Forms/DungeonForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataDrivenDungeon.Views
+{
+    /// <summary>
+    /// This is the form for all the dungeon fights. Depending on the dungeon they're in,
+    /// their inventory, and the enemies they're fighting, the data will change.
+    /// </summary>
+    public partial class DungeonForm : Form
+    {
+        public DungeonForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
