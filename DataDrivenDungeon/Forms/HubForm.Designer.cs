6b2ab91cccc7abcef8be76cee81718b5a606cf7f
Elias McKee <eqbookfan@gmail.com>
2023-07-26 18:24:07 -0700

    Linked the Hub form to all of the other elements.

--- DataDrivenDungeon/Forms/HubForm.Designer.cs
+++ DataDrivenDungeon/Forms/HubForm.Designer.cs
@@ -28,12 +28,97 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "HubForm";
+            SmithingBtn = new Button();
+            RepositoryBtn = new Button();
+            JackpotBtn = new Button();
+            BankBtn = new Button();
+            DungeonBtn = new Button();
+            QuitBtn = new Button();
+            SuspendLayout();
+            // 
+            // SmithingBtn
+            // 
+            SmithingBtn.Location = new Point(97, 130);
+            SmithingBtn.Name = "SmithingBtn";
+            SmithingBtn.Size = new Size(194, 84);
+            SmithingBtn.TabIndex = 0;
+            SmithingBtn.Text = "Stanley's Smithing";
+            SmithingBtn.UseVisualStyleBackColor = true;
+            SmithingBtn.Click += SmithingBtn_Click;
+            // 
+            // RepositoryBtn
+            // 
+            RepositoryBtn.Location = new Point(497, 130);
+            RepositoryBtn.Name = "RepositoryBtn";
+            RepositoryBtn.Size = new Size(194, 84);
+            RepositoryBtn.TabIndex = 1;
+            RepositoryBtn.Text = "Renaldo's Repository";
+            RepositoryBtn.UseVisualStyleBackColor = true;
+            RepositoryBtn.Click += RepositoryBtn_Click;
+            // 
+            // JackpotBtn
+            // 
+            JackpotBtn.Location = new Point(97, 314);
+            JackpotBtn.Name = "JackpotBtn";
+            JackpotBtn.Size = new Size(194, 84);
+            JackpotBtn.TabIndex = 2;
+            JackpotBtn.Text = "Jasper's Jackpot";
+            JackpotBtn.UseVisualStyleBackColor = true;
+            JackpotBtn.Click += JackpotBtn_Click;
+            // 
+            // BankBtn
+            // 
+            BankBtn.Location = new Point(497, 314);
+            BankBtn.Name = "BankBtn";
+            BankBtn.Size = new Size(194, 84);
+            BankBtn.TabIndex = 3;
+            BankBtn.Text = "The Bank";
+            BankBtn.UseVisualStyleBackColor = true;
+            BankBtn.Click += BankBtn_Click;
+            // 
+            // DungeonBtn
+            // 
+            DungeonBtn.Location = new Point(297, 130);
+            DungeonBtn.Name = "DungeonBtn";
+            DungeonBtn.Size = new Size(194, 84);
+            DungeonBtn.TabIndex = 4;
+            DungeonBtn.Text = "To The Dungeons";
+            DungeonBtn.UseVisualStyleBackColor = true;
+            DungeonBtn.Click += DungeonBtn_Click;
+            // 
+            // QuitBtn
+            // 
+            QuitBtn.Location = new Point(297, 314);
+            QuitBtn.Name = "QuitBtn";
+            QuitBtn.Size = new Size(194, 84);
+            QuitBtn.TabIndex = 5;
+            QuitBtn.Text = "Quit Game";
+            QuitBtn.UseVisualStyleBackColor = true;
+            QuitBtn.Click += QuitBtn_Click;
+            // 
+            // HubForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(QuitBtn);
+            Controls.Add(DungeonBtn);
+            Controls.Add(BankBtn);
+            Controls.Add(JackpotBtn);
+            Controls.Add(RepositoryBtn);
+            Controls.Add(SmithingBtn);
+            Name = "HubForm";
+            Text = "Village Hub";
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private Button SmithingBtn;
+        private Button RepositoryBtn;
+        private Button JackpotBtn;
+        private Button BankBtn;
+        private Button DungeonBtn;
+        private Button QuitBtn;
     }
 }
--- DataDrivenDungeon/Forms/HubForm.cs
+++ DataDrivenDungeon/Forms/HubForm.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DataDrivenDungeon.Views;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -20,15 +21,50 @@ namespace DataDrivenDungeon
             InitializeComponent();
         }
 
-        // connect to the smithing shop form
+        private void BankBtn_Click(object sender, EventArgs e)
+        {
+            // connect to the bank
+            BankForm bankForm = new BankForm();
+            bankForm.ShowDialog();
+        }
 
-        // connect to the repository shop form
+        private void JackpotBtn_Click(object sender, EventArgs e)
+        {
+            // connect to the jackpot shop form
+            JackpotStoreForm jackpotStoreForm = new JackpotStoreForm();
+            jackpotStoreForm.ShowDialog();
+        }
 
-        // connect to the jackpot shop form
+        private void RepositoryBtn_Click(object sender, EventArgs e)
+        {
+            // connect to the repository shop form
+            RepositoryStoreForm repositoryStoreForm = new RepositoryStoreForm();
+            repositoryStoreForm.ShowDialog();
+        }
 
-        // connect to the bank
+        private void SmithingBtn_Click(object sender, EventArgs e)
+        {
+            // connect to the smithing shop form
+            SmithingStoreForm smithingStoreForm = new SmithingStoreForm();
+            smithingStoreForm.ShowDialog();
+        }
 
-        // close the program
+        private void DungeonBtn_Click(object sender, EventArgs e)
+        {
+            // connect to the dungeon form
+            DungeonForm dungeonForm = new DungeonForm();
+            dungeonForm.ShowDialog();
+        }
+
+        private void QuitBtn_Click(object sender, EventArgs e)
+        {
+            // close the program
+            Close();
+        }
+
+
+
+
 
         // save the game as a JSON string?
     }
