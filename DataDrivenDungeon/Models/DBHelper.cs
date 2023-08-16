e0e74ab3bbdfabbe3fef001da0b017def51ab8e2
Elias McKee <eqbookfan@gmail.com>
2023-08-16 13:20:43 -0700

    Added DBHelper and stubbed-out get methods

---
+++ DataDrivenDungeon/Models/DBHelper.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataDrivenDungeon.Models
+{
+    public class DBHelper
+    {
+        // gets
+        public Weapon GetWeapon()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Armor GetArmor()
+        {
+            throw new NotImplementedException();
+        }
+
+        public GameData GetGameData()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Inventory GetInventory()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Dungeon GetDungeon()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Creature GetCreature()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
